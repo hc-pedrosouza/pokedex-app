7fd06c88de8d1f99310e68d1b9ad71a505709e48
pedrohces <hc.pedrosouza@gmail.com>
2023-03-24 15:45:10 -0300

    revert: 1

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,30 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { forkJoin } from 'rxjs';
-import { PokeApiService } from 'src/app/service/poke-api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss'],
+  styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
-  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
-  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species'
-  constructor(private activatedRoute: ActivatedRoute, private pokeApiService: PokeApiService) {}
+export class DetailsComponent {
 
-  ngOnInit(): void {
-    this.pokemon;
-  }
-
-  get pokemon() {
-    const id = this.activatedRoute.snapshot.params['id']
-    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`)
-    const name = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`)
-
-    return forkJoin([pokemon, name]).subscribe(
-      resp => {
-      }
-    )
-  }
 }
