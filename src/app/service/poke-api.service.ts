76ce5af3c7a9a8306bb2954dbb78d1de3ced6efa
pedrohces <hc.pedrosouza@gmail.com>
2023-03-24 14:18:54 -0300

    feat: using api data on list

---
+++ src/app/service/poke-api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon';
+
+  constructor(private http: HttpClient) {}
+
+  get apiListAllPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      //tap((resp) => resp),
+      tap((resp) => {
+        resp.results.map((respPokemons: any) => {
+          this.apiGetPokemons(respPokemons.url).subscribe( resp => respPokemons.status = resp)
+
+        })
+      })
+    );
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(
+      map(
+        resp => resp
+      )
+    )
+  }
+}
--- src/app/shared/pokedex-list/pokedex-list.component.ts
+++ src/app/shared/pokedex-list/pokedex-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'pokedex-list',
   templateUrl: './pokedex-list.component.html',
-  styleUrls: ['./pokedex-list.component.scss']
+  styleUrls: ['./pokedex-list.component.scss'],
 })
 export class PokedexListComponent {
+  constructor(private pokeApiService: PokeApiService) {}
 
+  public getAllPokemons: undefined;
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe((resp) => {
+      this.getAllPokemons = resp.results;
+      console.warn(this.getAllPokemons);
+    });
+  }
 }
